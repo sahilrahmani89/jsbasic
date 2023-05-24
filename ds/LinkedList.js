e24c0dd8fc02f35d9110d139665559111c8f0f66
Sahil Rahmani <sahilrahmani89@gmail.com>
2023-05-25 03:38:20 +0530

    add linkedlist with tail

--- ds/LinkedList.js
+++ ds/LinkedList.js
@@ -202,9 +202,9 @@ const list = new LinkedList()
 list.append(1)
 list.append(2)
 list.append(3)
-console.log(list.print())
+// console.log(list.print())
 list.reverse()
-console.log(list.print())
+// console.log(list.print())
 
 
 
