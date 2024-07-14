1fac40b00c090a958d3813a57c0c454bf8fc090c
SAHIL RAHMANI <sahilrahmani89@gmail.com>
2024-07-14 10:25:18 +0530

    update question

---
+++ practice/index.js
@@ -0,0 +1,13 @@
+{
+    //moves zero to end 
+    // [8,10,0,10,0,20] = [8,10,10,20,0,0] 
+    let arr = [8,10,0,0,0,10,0,20] 
+    let c = 0
+    for(let i =0;i<arr.length;i++){
+        if(arr[i]!==0){
+            [arr[i],arr[c]] = [arr[c],arr[i]]
+            c++
+        }
+    }
+    console.log(arr)
+}
