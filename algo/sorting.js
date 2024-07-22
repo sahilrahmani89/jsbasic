1c933932dc0bdf68fd732f26806ac82fd3ed82e4
SAHIL RAHMANI <sahilrahmani89@gmail.com>
2024-07-23 03:04:49 +0530

    updated  quicksort

--- algo/sorting.js
+++ algo/sorting.js
@@ -79,5 +79,68 @@
 }
 {
     // quick sort 
-    
+    // worst time complexity, ON^2
+    // avg time complexity O(NlogN)
+    // how it works 
+    // make pivot point (middle point of array) it can be any position of an array
+    // then divide into two parts left and right compare with pivot point and push it respectively.
+    // make recursively call for left and right 
+    // base case if arr.length less than 2 then return arr 
+
+    const arr = [4,1,3,2,2,3,10,1]
+    function quickSort(arr){
+        if(arr.length<2){
+            return arr
+        }
+        const pivotPoint =  arr[arr.length-1]
+        const left = []
+        const right = []
+        for(let i =0; i<arr.length-1; i++){
+            if(arr[i]>pivotPoint){
+                right.push(arr[i])
+            }else{
+                left.push(arr[i])
+            }
+        }
+        return [...quickSort(left),pivotPoint,...quickSort(right)]
+    }
+    // console.log('quickSort arr',quickSort(arr))
+}
+{
+    // merge sort 
+    // worst time complexity O(NLogN)
+    // how its work 
+    // divide the array into subarray , each containing one element (An array w one el considered to
+    // be sorted)
+    // Repeateadly merge the sub array to produce new sorted subarray  until there's one sub
+    // array remaining . That will be sorted array.
+    // two function to execute
+    // first find mid of the array , divide into left and right 
+    // do it until it converts into one single length of array
+    //  base case arr.length > 2
+    // merge function 
+    // temp array which store arr based on condition where shift data if 
+    // lesser and greater
+    const arr = [4,1,3,2,2,3,10,1]
+    function mergeSort(arr){
+        if(arr.length<2){
+            return arr
+        }
+        let mid = Math.floor(arr.length/2)
+        let left = arr.slice(0,mid)
+        let right = arr.slice(mid)
+        return merge(mergeSort(left),mergeSort(right))
+    }
+    function merge(left,right){
+        let temp = []
+        while(left.length && right.length){
+            if(left[0]<=right[0]){
+                temp.push(left.shift())
+            }else{
+                temp.push(right.shift())
+            }
+        }
+        return[...temp,...left,...right]
+    }
+    // console.log('mergeSort(arr)',mergeSort(arr))
 }
